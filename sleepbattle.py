8a4a1c71f8eac1967b19f806e4521975984a5e0e
sr8e <sr@mell0w-5phere.net>
2021-09-10 03:51:13 +0900

    create the frame of bot

---
+++ sleepbattle.py
@@ -0,0 +1,47 @@
+import re
+import os
+
+import discord
+
+
+DISCORD_TOKEN = os.environ.get("DISCORDBOT_TOKEN")
+
+WAKEPATTERN = r"^(?:[お起]きた|起床|おはよう)(.+)?"
+SLEEPPATTERN = r"^(?:[ね寝]る|就寝|おやすみ|ぽやしみ)(.+)?"
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is ready.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    channel = message.channel
+
+    if isinstance(channel, discord.DMChannel):
+        pass
+        # todo: surprise attack
+
+    elif isinstance(channel, discord.TextChannel) and channel.name == '起床報告':
+        if (match_w := re.match(WAKEPATTERN, message.content)) is not None:
+            if (overwrite_w := match_w.group(1)) is not None:
+                pass
+                # todo: overwrite
+            else:
+                pass
+                # todo: calculate and store the score
+
+        elif (match_s := re.match(SLEEPPATTERN, message.content)) is not None:
+            if (overwrite_s := match_s.group(1)) is not None:
+                pass
+                # todo: overwrite
+            else:
+                pass
+                # todo: memory the time
+
+client.run(DISCORD_TOKEN)
