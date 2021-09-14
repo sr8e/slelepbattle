6bf09a84a18091b2ce62271af15024c49c665ad8
sr8e <sr@mell0w-5phere.net>
2021-09-14 17:21:06 +0900

    add score calculation function

--- sleepbattle.py
+++ sleepbattle.py
@@ -1,7 +1,7 @@
 import re
 import os
 
-from datetime import timezone
+from datetime import datetime, time, timedelta, timezone
 
 import discord
 
@@ -13,6 +13,25 @@ WAKEPATTERN = r"^(?:[お起]きた|起床|おはよう)(.+)?"
 SLEEPPATTERN = r"^(?:[ね寝]る|就寝|おやすみ|ぽやしみ)(.+)?"
 
 client = discord.Client()
+
+
+def calculate_score(sleeptime, waketime, lastwaketime):
+    date = (waketime + timedelta(hours=5, minutes=30)).date()
+    sleep_f = datetime.combine(date - timedelta(days=1), time(hour=22, tzinfo=TIMEZONE))
+    sleep_l = datetime.combine(date, time(hour=1, tzinfo=TIMEZONE))
+    wake_f = datetime.combine(date, time(hour=5, tzinfo=TIMEZONE))
+    wake_l = datetime.combine(date, time(hour=8, tzinfo=TIMEZONE))
+
+    TD_ZERO = timedelta()
+    TD_HABIT = timedelta(hours=3)
+    TD_WIDTH = timedelta(hours=10)
+
+    sleep_score = max(1 - max(sleep_f - sleeptime, sleeptime - sleep_l, TD_ZERO) / TD_WIDTH, 0)
+    wake_score = max(1 - max(wake_f - waketime, waketime - wake_l, TD_ZERO) / TD_WIDTH, 0)
+    habit_score = 1 if abs(lastwaketime + timedelta(days=1) - waketime) < TD_HABIT else 0.9
+    print(f"{sleep_score}, {wake_score}, {habit_score}")
+
+    return date, 100 * sleep_score * wake_score * habit_score
 
 
 @client.event
