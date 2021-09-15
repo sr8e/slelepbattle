f6e6bdfeea30c11abbe54c2b97af7ac8897e36ae
sr8e <sr@mell0w-5phere.net>
2021-09-15 15:30:14 +0900

    rewrite db.py as context manager

--- db.py
+++ db.py
@@ -3,13 +3,44 @@ import psycopg2
 from settings import DATABASE_URL, DATETIME_FORMAT
 
 
-def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+class DBManager:
 
+    def __init__(self):
+        self.conn = self.get_connection()
 
-def insert_sleeptime(uid, sleeptime):
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute(f"insert into sleeptime (uid, sleeptime) values ({uid}, '{sleeptime.strftime(DATETIME_FORMAT)}');")
-            conn.commit()
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc_args):
+        self.conn.close()
+
+    @staticmethod
+    def get_connection():
+        return psycopg2.connect(DATABASE_URL)
+
+    def insert_sleeptime(self, uid, sleeptime, post_id):
+        with self.conn.cursor() as cur:
+            cur.execute(f"insert into sleeptime (uid, sleeptime, post_id) values ({uid}, '{sleeptime.strftime(DATETIME_FORMAT)}', {post_id});")
+            self.conn.commit()
+
+    def is_last_sleep_completed(self, uid):
+        with self.conn.cursor() as cur:
+            res_sleeptime = self.get_last_sleep(uid)
+            if res_sleeptime is None:
+                return True
+            cur.execute(f"select sleep_pk from score where uid={uid} order by id desc;")
+            res_score = cur.fetchone()
+            if res_score is None:
+                return False
+            return res_sleeptime[0] == res_score[0]
+
+    def get_last_sleep(self, uid):
+        with self.conn.cursor() as cur:
+            cur.execute(f"select id, sleeptime, post_id from sleeptime where uid={uid} order by id desc;")
+            return cur.fetchone()
+
+    def get_last_wake(self, uid):
+        with self.conn.cursor() as cur:
+            cur.execute(f"select id, waketime, post_id from waketime where uid={uid} order by id desc;")
+            return cur.fetchone()
 
