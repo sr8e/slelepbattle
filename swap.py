a46719742d52d60a78b8f7ee696ba42dbcbe9c44
sr8e <sr@mell0w-5phere.net>
2021-10-15 00:16:28 +0900

    add export_table function

--- swap.py
+++ swap.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import discord
 
@@ -56,6 +56,56 @@ async def swap():
                 await channel.send(f"{my_user.name} -> {target_user.name} の攻撃は、失敗しました。")
 
 
+async def export_table(prev=False):
+    channel = client.get_channel(NOTIFY_CHANNEL_ID)
+    d = datetime.now().astimezone(TIMEZONE).date()
+    if not prev and (d.weekday() + 1) < 2:
+        await export_table(True)
+
+    week_start = d - timedelta(days=(d.weekday() + 1) % 7)
+    if prev:
+        week_start -= timedelta(days=7)
+
+    with DBManager() as db:
+        scores = db.get_week_score(week_start)
+
+    scores_dict = {}
+    for s in scores:
+        if s.uid not in scores_dict:
+            scores_dict[s.owner] = {}
+        scores_dict[s.owner][s.date] = s
+
+    for owner, v in scores_dict.items():
+        scores_dict[owner]["avg"] = sum(map(lambda s: s.score, v.values())) / len(v)
+
+    owners_sorted = sorted(scores_dict, key=lambda k: scores_dict[k]["avg"], reverse=True)
+
+    week_val = "先" if prev else "今"
+    table = f"{week_val}週の順位表({d.strftime('%m/%d')}現在)```Pos | "
+    for i in range(7):
+        table += (week_start + timedelta(days=i)).strftime(" %m/%d ")
+    table += (
+        "|  Avg   | Name\n"
+        "====+==================================================+========+===============\n"
+    )
+    for i, owner in enumerate(owners_sorted):
+        user_scores = scores_dict[owner]
+        table += f"{i + 1:>2}. | "
+        for j in range(7):
+            key_date = week_start + timedelta(days=j)
+            if key_date not in user_scores:
+                table += "------ "
+            else:
+                s = user_scores[key_date]
+                score_str = f"{s.score:>6.2f}"
+                table += f"{score_str} " if s.owner == s.uid else f"{score_str}*"
+
+        table += f"| {user_scores['avg']:>6.2f} | {client.get_user(owner).name}\n"
+    table += "```"
+
+    await channel.send(table)
+
+
 async def reset_stock():
     channel = client.get_channel(NOTIFY_CHANNEL_ID)
     with DBManager() as db:
@@ -68,6 +118,7 @@ async def on_ready():
     print(f"{client.user} (for swap) is ready.")
     if action == "swap":
         await swap()
+        await export_table()
     elif action == "reset":
         await reset_stock()
     await client.close()
